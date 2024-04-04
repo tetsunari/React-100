0ed2c55c4456b935a8f7a1e4eaa6a8ae5e643212
tetsunari <mtst.tty556@gmail.com>
2024-04-04 16:26:19 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
-import React from "react";
 
-const AppWrapper = styled.div`
-    text-align: center;
-    height: 100vh;
+const Outer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    height: 100vh;
+    background-color: white;
+    border-radian: 100px;
+`;
+const Inner = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 2px solid black;
+    border-redius: 100px;
+    padding: 60px 100px;
+    background-color: white;
+`;
+const H1 = styled.h1`
+    font-weight: bold;
+    font-size: 50px;
+`;
+const Button = styled.button`
+    padding: 10px 20px;
+    margin: 20px;
+    border-radius: 100px;
+    font-weight: bold;
+    font-size: 25px;
+    cursor: pointer;
 `;
 
-const HelloWorld = styled.p`
-    font-size: 2rem;
-    color: blue;
-`;
-
-const App = () => {
+export default function App () {
+    const [count, setCount] = useState(0);
     return (
-        <AppWrapper>
-            <HelloWorld>Hello World!</HelloWorld>
-        </AppWrapper>
+        <Outer>
+            <Inner>
+                <h1><i>- Counter -</i></h1>
+                <H1>{count}</H1>
+                <div>
+                    <Button onClick={() => setCount(count + 1)}>+</Button>
+                    <Button onClick={() => setCount(count - 1)}>-</Button>
+                </div>
+            </Inner>
+        </Outer>
     );
-}
-
-export default App;
+};
